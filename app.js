64df7891b3c67cc78ebd4ae55e4d9ba5d78ec703
Demo User <demouser@MacBook-Air.local>
2015-07-23 09:29:12 -0400

    .

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#circle1").slideDown(4000);
+    $("#circle2").slideUp(4000);
+    $("#circle3").slideDown(4000);
+    // $("nav").fadeOut();
+    // $("nav").fadeIn();
+    // $("#photos").click(function(){
+    //   $("#photodropdown").fadeToggle();
+    // });
+    // $(".list").mouseenter(function(){
+    //   $(".list").css("background-color","black",);
+    // });
+});
