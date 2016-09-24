3047a5168210bdc9b253cea253b706fa8eb41397
CaylaVinzons <Cayla.vinzons@gmail.com>
2016-09-24 16:36:46 -0400

    this idea

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-$(document).ready(function(){
-
-   $(".bgimage").interactive_bg({
-       strength: 10,
-       scale: 1,
-       animationSpeed: "100ms",
-       contain: true,
-       wrapContent: false
-   }); // function call
-   $( "#content:hidden:first" ).fadeIn( 3000 );
-   $('#sidebar').slideDown();
-  //  $('a[data-mailto]').click(function(){
-  // var link = 'mailto.html#mailto:' + $(this).data('mailto');
-  // window.open(link, 'Mailer');
-  // return false;
-// });
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        anchors:['firstPage', 'secondPage', 'thirdPage']
+    });
 });
-
-
-
-$(window).resize(function() {
-    $(".bgimage > .ibg-bgimage").css({
-      width: $(window).outerWidth(),
-      height: $(window).outerHeight()
-    })
-  })
